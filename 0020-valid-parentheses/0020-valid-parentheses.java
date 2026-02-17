097ba3c60ac4e69e63d6b481b63697219f5189a1
Ayesha Khanum N <ayeshaxme@gmail.com>
2026-02-17 12:36:30 +0530

    Time: 3 ms (87.57%), Space: 43.3 MB (74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) 
+    { 
+
+     //it follows LIFO (stck) valid: {{[()]}}{[][]}
+     Stack<Character> stack = new Stack<>();
+     for(char c:s.toCharArray()){
+        if(c =='{') stack.push('}');
+        else if(c =='[') stack.push(']');
+        else if(c =='(') stack.push(')');
+        else{ 
+            if(stack.empty() || stack.pop()!=c)
+        return false;}
+     }
+    return stack.empty();
+
+    }
+}
